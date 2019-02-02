7fc4763a20dda5432bdfa6539f4a9b5150669051
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-02-02 15:54:39 +0600

    Evaluating Recommender System
    - Hit Rate
    - Cumulative Hit Rate
    - Rating Hit Rate
    - Average Reciprocal Hit Rate
    - User Coverage
    - Diversity
    - Novelty

--- Evaluating/RecommenderMetrics.py
+++ Evaluating/RecommenderMetrics.py
@@ -94,7 +94,67 @@ class RecommenderMetrics:
         # Compute overall precision
         for rating in sorted(hits.keys()):
             print (rating, hits[rating] / total[rating])
-                
-            # 
-                
+            
+    def AverageReciprocalHitRank(topNPredicted, leftOutPredictions):
+        summation = 0
+        total = 0
+        # For each left-out rating
+        for userID, leftOutMovieID, actualRating, estimatedRating, _ in leftOutPredictions:
+            # Is it in the predicted top N for this user?
+            hitRank = 0
+            rank = 0
+            for movieID, predictedRating in topNPredicted[int(userID)]:
+                rank = rank + 1
+                if (int(leftOutMovieID) == movieID):
+                    hitRank = rank
+                    break
+            if (hitRank > 0) :
+                summation += 1.0 / hitRank
+
+            total += 1
+
+        return summation / total
+
+    # What percentage of users have at least one "good" recommendation
+    def UserCoverage(topNPredicted, numUsers, ratingThreshold=0):
+        hits = 0
+        for userID in topNPredicted.keys():
+            hit = False
+            for movieID, predictedRating in topNPredicted[userID]:
+                if (predictedRating >= ratingThreshold):
+                    hit = True
+                    break
+            if (hit):
+                hits += 1
+
+        return hits / numUsers
+
+    def Diversity(topNPredicted, simsAlgo):
+        n = 0
+        total = 0
+        simsMatrix = simsAlgo.compute_similarities()
+        for userID in topNPredicted.keys():
+            pairs = itertools.combinations(topNPredicted[userID], 2)
+            for pair in pairs:
+                movie1 = pair[0][0]
+                movie2 = pair[1][0]
+                innerID1 = simsAlgo.trainset.to_inner_iid(str(movie1))
+                innerID2 = simsAlgo.trainset.to_inner_iid(str(movie2))
+                similarity = simsMatrix[innerID1][innerID2]
+                total += similarity
+                n += 1
+
+        S = total / n
+        return (1-S)
+
+    def Novelty(topNPredicted, rankings):
+        n = 0
+        total = 0
+        for userID in topNPredicted.keys():
+            for rating in topNPredicted[userID]:
+                movieID = rating[0]
+                rank = rankings[movieID]
+                total += rank
+                n += 1
+        return total / n                
                 
